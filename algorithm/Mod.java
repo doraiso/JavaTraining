60b0aa30ef6a7955ee2b5a5bad8ef29ab29240db
doraiso <doraiso.dora@gmail.com>
2018-08-11 08:12:38 +0900

    変数の名前変えたり

--- algorithm/Mod.java
+++ algorithm/Mod.java
@@ -14,25 +14,19 @@ public class Mod {
 	 */
 	static int[] arr = {1,4,6,5,2,0};
 
-
 	public static void main(String[] args) {
-
 		Mod m = new Mod();
-		Long keta = 1000000000000L; // 111....111(1兆桁)
-		System.out.println("111...111(" + keta + "桁)を7で割った余り is " + m.getSurplus(keta));
-
+		Long digit = 1000000000000L; // 111....111(1兆桁)
+		System.out.println("111...111(" + digit + "桁)を7で割った余り is " + m.getSurplus(digit));
 	}
 
 	/**
 	 * 111...111で割った余りを返す
-	 * @param keta 111...111の桁数
+	 * @param digit 111...111の桁数
 	 * @return 余り
 	 */
-	public int getSurplus(Long keta) {
-
-		int modKeta =  (int) (keta % arr.length - 1);
-		return arr[modKeta];
-
+	public int getSurplus(Long digit) {
+		int modDigit =  (int) (digit % arr.length - 1);
+		return arr[modDigit];
 	}
-
 }
