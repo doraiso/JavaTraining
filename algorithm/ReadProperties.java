94cbc8956b0a7d607dc8ef215a1d9adffd6920da
doraiso <doraiso.dora@gmail.com>
2020-11-02 01:47:00 +0900

    プロパティファイル読み込み動作確認

---
+++ algorithm/ReadProperties.java
@@ -0,0 +1,10 @@
+package algorithm;
+
+public class ReadProperties {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        System.out.println(PropertyUtil.getProperty("test.hoge")); // ⇒ テスト値
+	}
+
+}
